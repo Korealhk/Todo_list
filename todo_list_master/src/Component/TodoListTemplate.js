bb98c3c32b873ed32c0c88ff8abbaa43fd8cf082
Korealhk <dhmklhk0190@gmail.com>
2023-02-02 13:03:13 +0900

    - 제목 :  Templeate 제작
    - 내용 : Todo_list의 몸체와 헤더 제작
               - Templeate 제작
               - header 제작

---
+++ todo_list_master/src/Component/TodoListTemplate.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import "./TodoListTemplate.scss";
+
+class TodoListTemplate extends Component {
+  render() {
+    return (
+      <div className="TodoListTemplate">
+        1
+        <div className="app_header">
+         2   
+            <div className="data_area">3</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default TodoListTemplate;
