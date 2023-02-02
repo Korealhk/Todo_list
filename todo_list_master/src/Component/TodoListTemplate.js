4771ce1d2ee02e73b2b67e9f5fafbbff3e95bc2e
Korealhk <dhmklhk0190@gmail.com>
2023-02-02 13:57:10 +0900

    - 제목 :  Header 제작
    - 내용 : Header 제작
               - 날짜 데이터 출력

--- todo_list_master/src/Component/TodoListTemplate.js
+++ todo_list_master/src/Component/TodoListTemplate.js
@@ -2,16 +2,24 @@ import React from "react";
 import "./TodoListTemplate.scss";
 
 const TodoListTemplate = ({ children }) => {
+
+    const date = new Date();
+    const year = date.getFullYear();
+    const monthNames = ["JAN", "FAB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];
+    const month = monthNames[date.getMonth()];
+    const day = date.getDate();
+
+
   return (
     <div className="TodoListTemplate">
       <div className="app_header">
         <div className="data_area">
           <div className="left_col">
-            <div className="day_area">25</div>
+            <div className="day_area">{day}</div>
           </div>
           <div className="right_col">
-            <div className="month_area">7</div>
-            <div className="year_area">2023</div>
+            <div className="month_area">{month}</div>
+            <div className="year_area">{year}</div>
           </div>
         </div>
         <div className="title">Todo List</div>
